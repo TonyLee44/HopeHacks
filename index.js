6be600caabd92a4f4c064456ba8ee65222cc8ff0
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-02-16 12:19:00 -0500

    added weather api

--- index.js
+++ index.js
@@ -11,40 +11,40 @@ const apiKey = "f49dbbfa8e3250b4fa99956c3ea9d2eb";
 
 
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static('public'));
+// app.set('view engine', 'ejs')
+// app.use(bodyParser.urlencoded({extended : true}));
+// app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-    res.render('index');
-})
+// app.get('/', (req,res) => {
+//     res.render('index');
+// })
 
 
 
-app.post('/', (req,res) => {
-let city = req.body.city;
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+// app.post('/', (req,res) => {
+// let city = req.body.city;
+// let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-request(url, function(err,res,body) {
+// request(url, function(err,res,body) {
 
-    if(err) {
-        console.log(`Error: ${err}`)
-    } else {
-        // console.log(`Body: ${body}`)
+//     if(err) {
+//         console.log(`Error: ${err}`)
+//     } else {
+//         // console.log(`Body: ${body}`)
 
-        let weather = JSON.parse(body);
+//         let weather = JSON.parse(body);
 
-        let message = `It is ${weather.main.temp} degrees outside in ${weather.name}`;
+//         let message = `It is ${weather.main.temp} degrees outside in ${weather.name}`;
 
-        console.log(message)
+//         console.log(message)
 
 
         
-    }
+//     }
 
 
-})
-})
+// })
+// })
 
 
 
