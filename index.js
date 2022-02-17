79bb5309d20d3e05dc4b092a24dd4b90f9506d18
Samuel Rivas Ramirez <samuelrivasramirez@r2hs-mbp.lan>
2022-02-16 20:53:09 -0500

    AQI api first attempt

--- index.js
+++ index.js
@@ -3,45 +3,55 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-const apiKey = "f49dbbfa8e3250b4fa99956c3ea9d2eb";
+const apiKey = "e6f95f46a3dc82ac8e8ff584311ce59b"
+
+
+// let city = 'Chicago'
+
+// let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-    res.render('index');
+app.get('/', (req,res)=>{
+    res.render('index')
 })
 
-app.post('/', (req,res) => {
-let city = req.body.city;
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+app.post('/', (req,res)=>{
+    let city = req.body.city
 
-request(url, function(err,res,body) {
+    let url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=35.2271&lon=80.8431&appid=f49dbbfa8e3250b4fa99956c3ea9d2eb`
 
-    if(err) {
-        console.log(`Error: ${err}`)
-        res.render('index', {weather : null, error: err})
-    } else {
-        // console.log(`Body: ${body}`)
+    request(url, function(err,res,body){
 
-        let weather = JSON.parse(body);
+        if(err){
+            console.log(`Error: ${err}`)
+        } else{
+            console.log(`Body: ${body}`)
 
-        let message = `It is ${weather.main.temp} degrees outside in ${weather.name}`;
+            let list = JSON.parse(body)
 
-        console.log(message)
+            let message = `It is ${list.main.aqi} degrees outside`
+            console.log(message)
+        }
 
-
-        
-    }
-
-
+    })
 })
 
-})
+// request(url, function(err,res,body){
 
+//     if(err){
+//         console.log(`Error: ${err}`)
+//     } else{
+//         console.log(`Body: ${body}`)
 
+//         let weather = JSON.parse(body)
 
+//         let message = `It is ${weather.main.temp} degrees outside`
+//     }
+
+// })
 
 const port = 5000
 app.listen(port, ()=>{
