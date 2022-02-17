fe223860af97f8f255bf8e68fc0278b57d2fbcce
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-02-16 21:09:30 -0500

    Comment Tony's code

--- index.js
+++ index.js
@@ -56,3 +56,48 @@ const port = 5000
 app.listen(port, ()=>{
     console.log(`This server is running on ${port}`)
 })
+
+
+
+
+
+
+
+//////// Tony's code ////////
+/*
+const request = require("request");
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const apiKey = "f49dbbfa8e3250b4fa99956c3ea9d2eb";
+
+const app = {
+    init: () => {
+        document
+            .getElementById('btnGet')
+            .addEventListener('click', app.getLocation);
+        document
+            .getElementById('btnAirQuality')
+            .addEventListener('click', app.getAirQuality);
+    },
+    
+    getLocation: (ev) => {
+        
+    },
+
+    getAirQuality: (ev) => {
+
+    }
+}
+
+app.init();
+
+
+
+
+
+const port = 5000
+app.listen(port, ()=>{
+    console.log(`This server is running on ${port}`)
+})
+*/
